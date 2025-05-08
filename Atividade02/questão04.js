5c131616eb42d9adcb2a2c973d4c21afcdc18c55
Clara Sales <contaescolamariaclara@gmail.com>
2025-05-08 14:14:55 +0000

    atividade

--- Atividade02/questão04.js
+++ Atividade02/questão04.js
@@ -1,17 +1,17 @@
 function verificarOEmprestimo() {
     
     const salario = parseFloat(prompt("Digite o valor do seu salário:"));
-    const valorEmprestimo = parseFloat(prompt("Digite o valor do empréstimo desejado:"));
+    const valorEmprestimo = parseFloat(prompt("Digite o valor do emprestimo:"));
     const limiteEmprestimo = salario * 0.20;
   
     if (valorEmprestimo > limiteEmprestimo) {
-      console.log("Empréstimo não concedido: O valor do empréstimo excede 20% do seu salário.");
+      console.log("Emprestimo não concedido, empréstimo excede 20% do seu salário.");
     } 
     else {
-      console.log("Empréstimo pode ser concedido.");
+      console.log("Emprestimo pode ser concedido.");
     }
     console.log(`Salário: R$ ${salario.toFixed(2)}`);
-    console.log(`Valor do Empréstimo Solicitado: R$ ${valorEmprestimo.toFixed(2)}`);
-    console.log(`Limite de 20% do Salário para Empréstimo: R$ ${limiteEmprestimo.toFixed(2)}`);
+    console.log(`Valor do emprestimo solicitado: R$ ${valorEmprestimo.toFixed(2)}`);
+    console.log(`Limite de 20% do salário para realizar o emprestimo: R$ ${limiteEmprestimo.toFixed(2)}`);
   }
   verificarOEmprestimo();
